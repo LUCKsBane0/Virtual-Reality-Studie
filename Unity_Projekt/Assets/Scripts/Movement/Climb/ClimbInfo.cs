6e172448264ca13cd8ab0846e2c2ac4307a5b8be
felixtv0990 <felix.tv100@gmail.com>
2024-10-30 06:47:15 +0100

    SZ 2 MC
    Edited local prefab to "new" Ice-Axe
    Edited/added triggers to correspond with planned functionality
    Added ClimbInfo Script to note how  AxeClimb is supposed to work

---
+++ Unity_Projekt/Assets/Scripts/Movement/Climb/ClimbInfo.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+wenn BoxColliderAudioTip in die Eiswand eindringt/sie verlässt soll der entsprechende ton abgespielt werden
+BoxColiderSecureHold muss kontakt zur eiswand haben sonnst rutscht man ab
+Soll die Axt mit dem Stein kollidieren?
+Soll man den Griff der Axt in das Eis schieben können?
+
+Gewünschte Funktion:
+Input wird gegeben, überprüft ob BoxColiderSecureHold in Eiswand ist
+wenn ja, Axt fest spieler bewegt sich
+wenn nein, Axt nicht fest
+
+wenn eine axt fest ist und der spiler eine zweite fest machen will löst sich die erste wenn er erfolg hat
+-> wenn die zweite Axt nicht richtig im Berg ist fällt der Spieler nicht
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+public class ClimbInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
